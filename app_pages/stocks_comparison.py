80b288388843f155df6906c45c0a3b69d611729f
spate141 <isnehal.sp@gmail.com>
2023-08-27 09:29:15 -0500

    code optimization

--- app_pages/stocks_comparison.py
+++ app_pages/stocks_comparison.py
@@ -1,5 +1,6 @@
 import pandas as pd
 import streamlit as st
+from datetime import date
 from utils.stocks_util import *
 
 
@@ -8,18 +9,25 @@ def render_stocks_comparison_page():
     Render stock comparison page.
     """
     st.header('Stock Performance Comparison', divider='red')
+    current_date = date.today()
     with st.form("stocks"):
-        cols = st.columns([1, 1, 1, 1, 1])
+        cols = st.columns([1, 1, 1, 1, 1, 1])
         with cols[0]:
             company1 = st.text_input("Enter first company ticker:", "NVDA")
         with cols[1]:
             company2 = st.text_input("Enter second company ticker:", "SPY")
         with cols[2]:
-            invested_amount = st.number_input("Invested amount in USD:", min_value=0.0, value=1.0)
+            invested_amount = st.number_input("Invested amount in USD:", min_value=0, value=1000)
         with cols[3]:
-            start_date = st.date_input("Start date:", value=pd.to_datetime("2023-01-01"))
+            current_year = current_date.year
+            first_date_of_year = date(current_year, 1, 1)
+            formatted_first_date = first_date_of_year.strftime("%Y-%m-%d")
+            start_date = st.date_input("Start date:", value=pd.to_datetime(formatted_first_date))
         with cols[4]:
-            end_date = st.date_input("End date:", value=pd.to_datetime("2023-08-01"))
+            formatted_date = current_date.strftime("%Y-%m-%d")
+            end_date = st.date_input("End date:", value=pd.to_datetime(formatted_date))
+        with cols[5]:
+            trendline_type = st.selectbox("Trendline:", index=0, options=['exponential', 'linear'])
 
         submit = st.form_submit_button('Submit')
         if submit:
@@ -35,9 +43,10 @@ def render_stocks_comparison_page():
             merged_data = pd.concat([investment1, investment2], axis=1)
             merged_data.columns = [f"{company1} Investment", f"{company2} Investment"]
             final_investment1, final_investment2 = round(investment1[-1], 2), round(investment2[-1], 2)
-            st.markdown(
-                f"<b>{company1}:</b> \\${final_investment1} | <b>{company2}:</b> \\${final_investment2}",
-                unsafe_allow_html=True
-            )
-            fig = generate_plotly_chart(company1, company2, investment1, investment2)
+            cols = st.columns([0.8, 0.6, 0.6, 0.8])
+            with cols[1]:
+                st.metric(company1, f"${int(final_investment1):,}", f"{int(final_investment1-invested_amount):,}")
+            with cols[2]:
+                st.metric(company2, f"${int(final_investment2):,}", f"{int(final_investment2-invested_amount):,}")
+            fig = generate_plotly_chart(company1, company2, investment1, investment2, trendline_type=trendline_type)
             st.plotly_chart(fig, use_container_width=True)
--- utils/stocks_util.py
+++ utils/stocks_util.py
@@ -1,3 +1,4 @@
+import numpy as np
 import yfinance as yf
 import plotly.graph_objects as go
 from plotly.subplots import make_subplots
@@ -9,6 +10,7 @@ def get_stock_data(ticker, start, end):
     :param ticker: stock ticker
     :param start: start date
     :param end: end date
+    :return Pandas series
     """
     stock_data = yf.download(ticker, start=start, end=end)['Adj Close']
     return stock_data
@@ -17,21 +19,57 @@ def get_stock_data(ticker, start, end):
 def normalize_stock_data(data):
     """
     Normalize stock data
+    :param data: Pandas series
+    :return Normalized series
     """
     return (data / data.iloc[0] - 1) * 100
 
 
-def generate_plotly_chart(company1, company2, investment1, investment2):
+def generate_plotly_chart(company1, company2, investment1, investment2, trendline_type=None):
     """
-
+    Generate line chart using Plotly.
+    :param company1: Company 1 stock ticker
+    :param company2: Company 2 stock ticker
+    :param investment1: Company 1 invested amount series
+    :param investment2: Company 2 invested amount series
+    :param trendline_type: Chart trend-line type
+    :return Plotly figure
     """
     # Initialize Plotly figure
     fig = make_subplots(rows=1, cols=1)
-    fig.add_trace(go.Scatter(x=investment1.index, y=investment1.values, mode='lines', name=f"{company1} Investment"))
-    fig.add_trace(go.Scatter(x=investment2.index, y=investment2.values, mode='lines', name=f"{company2} Investment"))
+
+    # Add investment lines for both companies
+    fig.add_trace(go.Scatter(x=investment1.index, y=investment1.values, mode='lines', name=f"{company1}"))
+    fig.add_trace(go.Scatter(x=investment2.index, y=investment2.values, mode='lines', name=f"{company2}"))
+
+    # Calculate average investment values
+    avg_investment1 = np.mean(investment1.values)
+    avg_investment2 = np.mean(investment2.values)
+
+    # Add average lines for both companies
+    fig.add_trace(go.Scatter(x=investment1.index, y=[avg_investment1] * len(investment1.index),
+                             mode='lines', name=f"Avg {company1}", line=dict(dash='dash')))
+    fig.add_trace(go.Scatter(x=investment2.index, y=[avg_investment2] * len(investment2.index),
+                             mode='lines', name=f"Avg {company2}", line=dict(dash='dash')))
+
+    # Add trendlines based on the selected trendline_type
+    if trendline_type == "exponential":
+        trendline1 = np.polyfit(np.arange(len(investment1)), np.log(investment1.values), 1)
+        trendline2 = np.polyfit(np.arange(len(investment2)), np.log(investment2.values), 1)
+        fig.add_trace(go.Scatter(x=investment1.index, y=np.exp(np.polyval(trendline1, np.arange(len(investment1)))),
+                                 mode='lines', name=f"Exp Trend {company1}", line=dict(dash='dot')))
+        fig.add_trace(go.Scatter(x=investment2.index, y=np.exp(np.polyval(trendline2, np.arange(len(investment2)))),
+                                 mode='lines', name=f"Exp Trend {company2}", line=dict(dash='dot')))
+    elif trendline_type == "linear":
+        trendline1 = np.polyfit(np.arange(len(investment1)), investment1.values, 1)
+        trendline2 = np.polyfit(np.arange(len(investment2)), investment2.values, 1)
+        fig.add_trace(go.Scatter(x=investment1.index, y=np.polyval(trendline1, np.arange(len(investment1))),
+                                 mode='lines', name=f"Linear Trend {company1}", line=dict(dash='dot')))
+        fig.add_trace(go.Scatter(x=investment2.index, y=np.polyval(trendline2, np.arange(len(investment2))),
+                                 mode='lines', name=f"Linear Trend {company2}", line=dict(dash='dot')))
+    # Add more trendline types here (logarithmic, power, polynomial, moving average)
 
     # Set x-axis ticks and get tick positions
-    # Here, it takes every nth index where n = total length // 10
     tickvals = investment1.index[::len(investment1.index) // 10]
     fig.update_xaxes(tickvals=tickvals)
 
@@ -39,14 +77,17 @@ def generate_plotly_chart(company1, company2, investment1, investment2):
     for tick in tickvals:
         y_val1 = investment1.loc[tick]
         y_val2 = investment2.loc[tick]
-        fig.add_annotation(x=tick, y=y_val1, text=f"<b>${y_val1:.2f}</b>", showarrow=False)
-        fig.add_annotation(x=tick, y=y_val2, text=f"<b>${y_val2:.2f}</b>", showarrow=False)
+        fig.add_annotation(x=tick, y=y_val1, text=f"<b>${y_val1:.0f}</b>", showarrow=False)
+        fig.add_annotation(x=tick, y=y_val2, text=f"<b>${y_val2:.0f}</b>", showarrow=False)
 
     # Label and render the figure
     fig.update_layout(
-        # title="Investment Performance Comparison:",
         yaxis_title='Investment Value (USD)',
         xaxis_title='Date',
-        height=700
+        height=700,
+        font=dict(
+            size=14,
+            color="RebeccaPurple"
+        )
     )
     return fig
