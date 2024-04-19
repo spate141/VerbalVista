816c200d937db9037036ab6178c64205abd4ae83
spate141 <isnehal.sp@gmail.com>
2024-04-18 21:06:45 -0500

    Updated version 3.8

--- server.py
+++ server.py
@@ -22,7 +22,7 @@ from utils.data_parsing_utils.reddit_comment_parser import RedditSubmissionComme
 app = FastAPI(
     title="Inference API for VerbalVista",
     description="🄻🄻🄼 + Your Data = ♥️",
-    version="3.3",
+    version="3.8",
 )
 auth_util = AuthUtil(
     valid_api_keys=os.getenv("VALID_API_KEYS", "").split(",")
